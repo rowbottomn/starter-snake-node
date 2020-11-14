72f7f8457bef5eb8f1537e0f050e50177942de94
Nathan Rowbottom <nathan.rowbottom@snpsteam.com>
2020-11-14 21:06:49 +0000

    removed dead code and refined verbose logging

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var food = [];
 
 var head = {};
 var heading = 0;
-var verbose = true;
+var verbose = false;
 
 //making attracted to food and repeled from other snakes head
 
@@ -56,12 +56,7 @@ function handleStart(request, response) {
   x = me.head.x;
   y = me.head.y;
   
- // console.log(x+", "+y);
-  for (var i = 0; i < snakes.length; i++){
- //     console.log(snakes[i].head.x+", "+snakes[i].head.y);
-  }
-
-  console.log('START')
+  log('START')
   response.status(200).send('ok')
 }
 
@@ -69,15 +64,15 @@ function getHeading(x1, y1, x2, y2){
 
   var dx = x1 - x2;
   var dy = y1 - y2;
-  if (verbose){
-    console.log("x1 y1 " + x1+ ", "+y1);
-    console.log("x2 y2 " + x2+ ", "+y2);
-    console.log("dx dy " + dx+ ", "+dy);
-  }
+  
+  log("x1 y1 " + x1+ ", "+y1);
+  log("x2 y2 " + x2+ ", "+y2);
+  log("dx dy " + dx+ ", "+dy);
+  
   
   var direction = 0;
   if (dx===0 && dy===0){
-    console.log("wtf");
+    log("wtf");
     return 0;
   }
   if (dx === 0 ){
@@ -88,7 +83,7 @@ function getHeading(x1, y1, x2, y2){
       direction = 0; //up
     }
     else{
-      console.log("wtf");
+      log("wtf");
     }
   }
   else if (dy === 0 ){
@@ -99,7 +94,7 @@ function getHeading(x1, y1, x2, y2){
       direction = 1; //right
     }
     else{
-      console.log("wtf");
+      log("wtf");
     }
   }
 
@@ -184,7 +179,7 @@ function getWeightings(x, y, s, f, w){
       var distance = getManhattenDistance(xs[i], ys[i], cornerXs[j] , cornerYs[j] );
       w[i]+= Math.max(cornerScalar/(distance*distance), weightLimits[3]);
     }
-    console.log("weightings"+i+": "+w[i]);
+    log("weightings"+i+": "+w[i]);
   }
   return w;
 }
@@ -202,7 +197,7 @@ function handleMove(request, response) {
   x = head.x;
   y = head.y;
   heading = getHeading(x, y, body[1].x, body[1].y);
-  console.log("heading is "+ possibleMoves[heading]);
+  log("heading is "+ possibleMoves[heading]);
 
   weightings = getWeightings(x, y, snakes, food, weightings);
   var best = heading;
@@ -213,114 +208,10 @@ function handleMove(request, response) {
       bestWeighting = weightings[i];
     }
   }
-    
-  //var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-  //look ahead to see what might happen
- // var newX = x;
- // var newY = y;
-  //var randomChance  = Math.floor(Math.random() * 100);
 
-  /*
-  if (me.health< 20){
-    
-    heading =Math.floor(Math.random() * possibleMoves.length);
-    if (verbose){
-      console.log("random!");
-    }
-  }
-  if (heading === 0 ){//if going up
-    
-    if (!isMoveSafe(x, y+1, snakes)){
-      //try left
-      if (isMoveSafe(x-1, y, snakes)){
-        heading = 3;
-      }
-      else if(isMoveSafe(x+1, y, snakes)){
-        heading = 1;
-      }
-      else if(isMoveSafe(x, y-1, snakes)){
-        heading = 2;
-      }
-    }
-  }
-  else if (heading === 2 ){//if going down
-    
-    if (!isMoveSafe(x, y-1, snakes)){//can we still go down
-      //try left
-      if (isMoveSafe(x-1, y, snakes)){
-        heading = 3;
-      }
-      else if(isMoveSafe(x+1, y, snakes)){
-        heading = 1;
-      }
-      else if(isMoveSafe(x, y+1, snakes)){
-        heading = 0;
-      }
-
-    }
-  }
-
-  else if (heading === 1 ){//if going right
-    
-    if (!isMoveSafe(x+1, y, snakes)){
-      //try left
-      if (isMoveSafe(x, y+1, snakes)){
-        heading = 0;
-      }
-      else if(isMoveSafe(x, y-1, snakes)){
-        heading = 2;
-      }
-      else if(isMoveSafe(x-1, y, snakes)){
-        heading = 3;
-      }
-
-    }
-  }
-  else if (heading === 3 ){//if going left
-    
-    if (!isMoveSafe(x-1, y, snakes)){
-      //try left
-      if (isMoveSafe(x, y+1, snakes)){
-        heading = 0;
-      }
-      else if(isMoveSafe(x, y-1, snakes)){
-        heading = 2;
-      }
-      else if(isMoveSafe(x+1, y, snakes)){
-        heading = 1;
-      }
-
-    }
-  } 
-  /*
-  if (heading === 0 ){//if going up
-    newY ++; //our newY is one higher
-    if (newY >=11){//if the newY is off the top of the screen 
-      heading = 3;//go left
-    } 
-  }
-  else if (heading === 2){
-    newY --;
-    if (newY <0){
-      heading = 1;
-    } 
-  }
-  else if (heading === 1 ){
-    newX ++;
-    if (newX >=11){
-      heading = 0;
-    } 
-  }
-  else if (heading === 3){
-    newX --;
-    if (newX <0){
-      heading = 2;
-    } 
-  }
-  */
 
   var move = possibleMoves[best];
-  console.log('MOVE: ' + move)
+  log('MOVE: ' + move)
   response.status(200).send({
     move: move
   })
@@ -329,6 +220,6 @@ function handleMove(request, response) {
 function handleEnd(request, response) {
   var gameData = request.body
 
-  console.log('END '+gameData.turn)
+  log('END '+gameData.turn)
   response.status(200).send('ok')
 }
